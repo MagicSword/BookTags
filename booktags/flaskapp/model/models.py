987def95ca5c54442e4acd72f304b40deca9ada2
Nero Miller <ming927@spp.url.com.tw>
2019-12-20 22:21:40 +0800

    # Flask web development 2/e ch11 11f
    *  Rich text server side handling with Markdown and Bleach (11f)

--- booktags/flaskapp/model/models.py
+++ booktags/flaskapp/model/models.py
@@ -16,10 +16,13 @@
 #     path will be displayed.
 from datetime import datetime
 import hashlib
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app, request
 from flask_login import UserMixin, AnonymousUserMixin
+from markdown import markdown
+import bleach
 from .. import db, login_manager
 # --------------------------------------------------------- common routines
 
@@ -245,6 +248,17 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
