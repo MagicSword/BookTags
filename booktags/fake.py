d33ce07aed636a8c6f79e52d57ad851b802f2aac
Nero Miller <ming927@spp.url.com.tw>
2019-12-20 20:52:23 +0800

    # Flask web development 2/e ch11 11c
    * Generate fake users and posts (11c)
    * requirements 拆成目錄，三檔
    * faker, 產生文章

---
+++ booktags/fake.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    example.py
+    ~~~~~~~~~
+    A simple command line application to run flask apps.
+    :copyright: 2019 Miller
+    :license: BSD-3-Clause
+"""
+
+# Known bugs that can't be fixed here:
+#   - synopsis() cannot be prevented from clobbering existing
+#     loaded modules.
+#   - If the __file__ attribute on a module is a relative path and
+#     the current directory is changed with os.chdir(), an incorrect
+#     path will be displayed.
+
+from random import randint
+from sqlalchemy.exc import IntegrityError
+from faker import Faker
+from .flaskapp import db
+from .flaskapp.model.models import User, Post
+# --------------------------------------------------------- common routines
+
+def users(count=100):
+    fake = Faker()
+    i = 0
+    while i < count:
+        u = User(email=fake.email(),
+                 username=fake.user_name(),
+                 password='password',
+                 confirmed=True,
+                 name=fake.name(),
+                 location=fake.city(),
+                 about_me=fake.text(),
+                 member_since=fake.past_date())
+        db.session.add(u)
+        try:
+            db.session.commit()
+            i += 1
+        except IntegrityError:
+            db.session.rollback()
+
+
+def posts(count=100):
+    fake = Faker()
+    user_count = User.query.count()
+    for i in range(count):
+        u = User.query.offset(randint(0, user_count - 1)).first()
+        p = Post(body=fake.text(),
+                 timestamp=fake.past_date(),
+                 author=u)
+        db.session.add(p)
+    db.session.commit()
